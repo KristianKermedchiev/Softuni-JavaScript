bd632cc8f4a7616262e2be7c23b936a737b9a475
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-14 14:56:52 +0300

    Create 01.fruit.js

---
+++ Softuni - Advanced course/01.Syntax, Functions and Statements/Exercises/01.fruit.js
@@ -0,0 +1,8 @@
+function fruit (type, weight, price){
+
+    let weightInKg = weight/1000;
+    let result = weightInKg * price;
+    console.log(`I need $${result.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${type}.`)
+}
+fruit('orange', 2500, 1.80);
+fruit('apple', 1563, 2.35);
