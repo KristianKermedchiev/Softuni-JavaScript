f44b8e1665849985c8d8225ec247307ca2e89b42
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-16 23:45:55 +0300

    Update 11.equalNeighbours.js

--- Softuni - Advanced course/02.Arrays and Nested Arrays/Lab/11.equalNeighbours.js
+++ Softuni - Advanced course/02.Arrays and Nested Arrays/Lab/11.equalNeighbours.js
@@ -1,8 +1,13 @@
 function equalNeighbours(matrix) {
     let neighbors = 0;
     for (let row = 0; row < matrix.length; row++) {
+        
         for (let col = 0; col < matrix[row].length; col++) {
+            let temp1 = matrix[row];
             if (row < matrix.length-1) {
+                let temp2 = row;
+                let temp3 = matrix[row][col];
+                let temp4 = matrix[row + 1][col];
                 if (matrix[row][col] == matrix[row + 1][col]) {
                     neighbors++;
                 }
