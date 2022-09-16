615eb4060c33344634b9e23516744cf6ce81b3fc
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-17 00:07:11 +0300

    Create 03.addAndRemoveElements.js

---
+++ Softuni - Advanced course/02.Arrays and Nested Arrays/Exercises/03.addAndRemoveElements.js
@@ -0,0 +1,41 @@
+function addAndRemoveElements (input){
+    
+    let result = [];
+    let num = 1;
+
+    for(let el of input){
+        switch(el){
+            case 'add':
+                result.push(num);
+                break;
+                case 'remove':
+                    result.pop();
+                    break;
+                    
+        }
+        num++;
+    }
+
+    if(result.length === 0){
+        return 'Empty'
+    } else {
+
+        return result.join('\n')
+    }
+
+}
+console.log(addAndRemoveElements(['add', 
+'add', 
+'add', 
+'add']
+));
+console.log(addAndRemoveElements(['add', 
+'add', 
+'remove', 
+'add', 
+'add']
+));
+console.log(addAndRemoveElements(['remove', 
+'remove', 
+'remove']
+));
