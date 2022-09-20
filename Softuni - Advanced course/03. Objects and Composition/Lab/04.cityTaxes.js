6d9d2b1d7d72949ab1f8812630e99dcab61b760b
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-20 19:50:10 +0300

    Create 04.cityTaxes.js

---
+++ Softuni - Advanced course/03. Objects and Composition/Lab/04.cityTaxes.js
@@ -0,0 +1,34 @@
+function cityTaxes(name, population, treasury) {
+    return {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percent) {
+            this.population += Math.floor(this.population * percent / 100);
+        },
+        applyRecession(percent) {
+            this.treasury -= Math.floor(this.treasury * percent / 100);
+        }
+    }
+}
+
+const city =
+    cityTaxes('Tortuga',
+        7000,
+        15000);
+console.log(city);
+
+console.log(`-----------------------------------`);
+
+const city2 =
+    cityTaxes('Tortuga',
+        7000,
+        15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
