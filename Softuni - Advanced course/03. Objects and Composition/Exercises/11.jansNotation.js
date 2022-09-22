4e43b77e8ec2304b2aee939ef638a3c6df6bf297
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-22 13:38:09 +0300

    8 and 11

--- Softuni - Advanced course/03. Objects and Composition/Exercises/11.jansNotation.js
+++ Softuni - Advanced course/03. Objects and Composition/Exercises/11.jansNotation.js
@@ -5,33 +5,31 @@ function jansNotation(arr) {
     let operators = [];
 
     let operationEnum = {
-        "+": (a,b) => a + b,
-        "-": (a,b) => a - b,
-        "*": (a,b) => a * b,
-        "/": (a,b) => a / b,
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => a / b,
     }
 
-    let boolean = false;
-
-    for(let el of arr){
+    for (let el of arr) {
         if (typeof (el) === 'number') {
             number.push(el);
         } else {
             operators.push(el);
         }
     }
-    
-    if(operators.length < number.length -1){
+
+    if (operators.length < number.length - 1) {
         console.log(`Error: too many operands!`);
         return;
-    } else if (number.length === operators.length || number.length === 0){
+    } else if (number.length === operators.length || number.length === 0) {
         console.log(`Error: not enough operands!`);
         return;
     }
-        
 
-    for(let el of arr){
-        if(typeof(el) === "number"){
+
+    for (let el of arr) {
+        if (typeof (el) === "number") {
             res.push(el);
             continue;
         }
