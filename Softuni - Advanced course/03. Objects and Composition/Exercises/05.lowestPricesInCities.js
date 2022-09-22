5caf7eb6db9485dc087bf0186e59ccd30cb69e14
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-22 13:32:13 +0300

    5 and 11

--- Softuni - Advanced course/03. Objects and Composition/Exercises/05.lowestPricesInCities.js
+++ Softuni - Advanced course/03. Objects and Composition/Exercises/05.lowestPricesInCities.js
@@ -1,34 +1,34 @@
-function lowestPrices (input){
+function lowestPrices(input) {
 
     let res = {};
 
-    for(let el of input){
+    for (let el of input) {
         let [town, product, price] = el.split(' | ');
         price = Number(price);
-        
-        if(res.hasOwnProperty(product)){
+
+        if (res.hasOwnProperty(product)) {
             let currentTown = res[product][town];
             let currentPrice = res[product]['price'];
-            if(currentPrice > price){
-                res[product] = {town, price};
+            if (currentPrice > price) {
+                res[product] = { town, price };
             }
         } else {
-            res[product] = {town, price};
+            res[product] = { town, price };
         }
     }
 
-    for(let [product, value] of Object.entries(res)){
-    
-    console.log(`${product} -> ${value.price} (${value.town})`)
-        
+    for (let [product, value] of Object.entries(res)) {
+
+        console.log(`${product} -> ${value.price} (${value.town})`)
+
     }
 
 }
 lowestPrices(['Sample Town | Sample Product | 1000',
-'Sample Town | Orange | 2',
-'Sample Town | Peach | 1',
-'Sofia | Orange | 3',
-'Sofia | Peach | 2',
-'New York | Sample Product | 1000.1',
-'New York | Burger | 10']
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10']
 );
---
+++ Softuni - Advanced course/03. Objects and Composition/Exercises/11.jansNotation.js
@@ -0,0 +1,65 @@
+function jansNotation(arr) {
+
+    let res = [];
+    let number = [];
+    let operators = [];
+
+    let operationEnum = {
+        "+": (a,b) => a + b,
+        "-": (a,b) => a - b,
+        "*": (a,b) => a * b,
+        "/": (a,b) => a / b,
+    }
+
+    let boolean = false;
+
+    for(let el of arr){
+        if (typeof (el) === 'number') {
+            number.push(el);
+        } else {
+            operators.push(el);
+        }
+    }
+    
+    if(operators.length < number.length -1){
+        console.log(`Error: too many operands!`);
+        return;
+    } else if (number.length === operators.length || number.length === 0){
+        console.log(`Error: not enough operands!`);
+        return;
+    }
+        
+
+    for(let el of arr){
+        if(typeof(el) === "number"){
+            res.push(el);
+            continue;
+        }
+
+        let numA = res.pop();
+        let numB = res.pop();
+        let result = operationEnum[el](numB, numA);
+        res.push(result);
+
+    }
+
+    console.log(res.join());
+}
+jansNotation([3,
+    4,
+    '+']
+);
+jansNotation([5,
+    3,
+    4,
+    '*',
+    '-']
+);
+jansNotation([7,
+    33,
+    8,
+    '-']
+);
+jansNotation([15,
+    '/']
+);
