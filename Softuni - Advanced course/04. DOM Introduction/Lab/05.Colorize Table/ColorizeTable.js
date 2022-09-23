cc3c5d3904ad602c6b4ce6152d0bf56c23bde773
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-23 13:31:53 +0300

    Update ColorizeTable.js

--- Softuni - Advanced course/04. DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ Softuni - Advanced course/04. DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    let rows = document.getElementsByTagName('tr');
+
+    for(let i = 1; i < rows.length; i++){
+        let row = rows[i];
+            if(i % 2 !== 0){
+            row.style.background = "teal";
+        }
+    }
 }
