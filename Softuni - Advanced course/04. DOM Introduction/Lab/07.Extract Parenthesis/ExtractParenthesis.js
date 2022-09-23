d33c549eb5d09c1ddabecbdba0b3e98c3acf3569
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-23 13:53:06 +0300

    Update ExtractParenthesis.js

--- Softuni - Advanced course/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ Softuni - Advanced course/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    let elementText = document.getElementById(content).textContent;
+    let pattern = /\(([^)]+)\)/g;
 
+    let result = elementText.matchAll(pattern);
+
+    let matches = [];
+
+    for(let text of result){
+        matches.push(text[1]);
+    }
+
+    return matches.join('; ');
 }
