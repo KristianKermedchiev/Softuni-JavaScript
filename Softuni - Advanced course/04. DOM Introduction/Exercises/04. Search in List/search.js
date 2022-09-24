f5bb6eaa14f04ce0335f9f1417f617adaa589873
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-24 14:26:04 +0300

    Update search.js

--- Softuni - Advanced course/04. DOM Introduction/Exercises/04. Search in List/search.js
+++ Softuni - Advanced course/04. DOM Introduction/Exercises/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   let townList = Array.from(document.querySelectorAll('ul li'));
+   let criteria = document.getElementById('searchText').value;
+
+   let matches = 0;
+   
+   for(let el of townList){
+      let city = el.textContent;
+      
+      if(city.includes(criteria)){
+         el.style.textDecoration = 'underline';
+         el.style.fontWeight = 'bold';
+         matches++;
+      }
+   }
+   document.getElementById('result').textContent = `${matches} matches found`;
 }
