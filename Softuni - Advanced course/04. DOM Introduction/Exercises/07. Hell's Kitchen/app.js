d316c9bda9a06c3106cbb9ec6b58a3d8ed3d9324
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-24 19:45:52 +0300

    Update app.js

--- Softuni - Advanced course/04. DOM Introduction/Exercises/07. Hell's Kitchen/app.js
+++ Softuni - Advanced course/04. DOM Introduction/Exercises/07. Hell's Kitchen/app.js
@@ -1,8 +1,52 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
-
+   let result = [];
    function onClick () {
-      //   TODO:
+      let input = JSON.parse(document.getElementById('inputs').children[1].value);
+      let bestRestaurantInfo = document.querySelector('#bestRestaurant p');
+      let bestWorker = document.querySelector('#workers p');
+
+      for(let data of input){
+         let [name, workerList] = data.split(' - ');
+         if(!result.find(e => e.name === name)){
+            result.push({
+               name,
+               avgSalary : 0,
+               bestSalary : 0,
+               sumSalary : 0,
+               workerList: []
+            });
+         }
+            let currentRestaurant = result.find(e=> e.name === name);
+            workerList = workerList && workerList.split(', ');
+            for(let currentWorker of workerList){
+               updateResaurant(currentRestaurant, currentWorker)
+            }
+         
+      }
+      let bestRestaurant = result.sort((a, b) => b.avgSalary - a.avgSalary)[0];
       
+      bestRestaurantInfo.textContent = `Name: ${bestRestaurant.name} Average Salary: ${bestRestaurant.avgSalary.toFixed(2)} Best Salary: ${bestRestaurant.bestSalary.toFixed(2)}`
+
+      let sortListOfWorker = bestRestaurant.workerList.sort((a, b)=> b.salary-a.salary);
+      let buff = '';
+      for(let worker of sortListOfWorker){
+         buff += `Name: ${worker.name} With Salary: ${worker.salary} `
+      }
+      bestWorker.textContent += buff;
+   }
+
+   function updateResaurant(obj, worker){
+      let  [name, salary] = worker.split(' ');
+      salary = Number(salary);
+      obj.sumSalary += Number(salary);
+      if(obj.bestSalary < salary){
+         obj.bestSalary = salary;
+      }
+      obj.workerList.push({
+         name,
+         salary
+      });
+      obj.avgSalary = obj.sumSalary / obj.workerList.length;
    }
 }
