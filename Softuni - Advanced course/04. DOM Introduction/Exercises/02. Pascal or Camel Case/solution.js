32ac5f5f36f55934e70d82f8111f18410b5e7a83
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-24 12:45:27 +0300

    Update solution.js

--- Softuni - Advanced course/04. DOM Introduction/Exercises/02. Pascal or Camel Case/solution.js
+++ Softuni - Advanced course/04. DOM Introduction/Exercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value;
+  let convention = document.getElementById('naming-convention').value;
+
+  let textArr = text.split(' ');
+  let result = '';
+  switch (convention) {
+    case 'Camel Case':
+      textArr.forEach((element, i) => {
+        if(i === 0){
+          return result += element.toLowerCase();
+        } else {
+          return result +=element[0].toUpperCase() + element.substring(1,).toLowerCase();
+        } 
+      });
+      break;
+    case 'Pascal Case':
+      textArr.forEach((element, i) => {
+        element = element.toLowerCase();
+        result += element[0].toUpperCase() + element.substring(1,).toLowerCase();
+      })
+      break;
+    default: result = 'Error!'
+    break;
+  }
+
+  document.getElementById('result').textContent = result;
+  
 }
