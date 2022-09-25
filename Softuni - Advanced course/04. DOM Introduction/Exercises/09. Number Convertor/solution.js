3c440015b29ce36510e3e78edee4d651cd385abb
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-25 13:55:22 +0300

    Update solution.js

--- Softuni - Advanced course/04. DOM Introduction/Exercises/09. Number Convertor/solution.js
+++ Softuni - Advanced course/04. DOM Introduction/Exercises/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
 
-    //TODO...
+    const selectMenu = document.getElementById('selectMenuTo');
+    const binaryOption = document.createElement('option');
+
+    binaryOption.textContent = 'Binary';
+    binaryOption.value = 'binary';
+    const hexadecimalOption = document.createElement('option');
+    hexadecimalOption.textContent = 'hexadecimal';
+    hexadecimalOption.value = 'hexadecimal';
+
+    selectMenu.appendChild(binaryOption);
+    selectMenu.appendChild(hexadecimalOption);
+
+    const selectMap = {
+        'binary': num => num.toString(2),
+        'hexadecimal': num => num.toString(16).toUpperCase()
+    }
+
+    const convertBtn = document.querySelector('#container > button');
+
+    convertBtn.addEventListener('click', (evt) => {
+        const inputData = document.getElementById('input');
+        const outputData = document.getElementById('result');
+
+        outputData.value = selectMap[selectMenu.value](+inputData.value);
+    })
 }
