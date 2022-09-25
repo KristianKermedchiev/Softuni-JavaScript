d29e0757ea705dfbd829d3c94baa1cecbf8c8fb9
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-25 13:45:45 +0300

    Update solution.js

--- Softuni - Advanced course/04. DOM Introduction/Exercises/05. Table - Search Engine/solution.js
+++ Softuni - Advanced course/04. DOM Introduction/Exercises/05. Table - Search Engine/solution.js
@@ -1,8 +1,26 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+   let input = document.getElementById('searchField');
+   let body = document.querySelectorAll('tbody tr');
 
+   if (input === null || body === null) {
+      throw new Error('Missing HTML elements!');
+   }
+
+   
+   function onClick() {
+      for (let tr of body) {
+         tr.classList.remove('select');
+      }
+      if (input.value == '') {
+         return '';
+      }
+      for (let tr of body) {
+         if (tr.textContent.includes(input.value)) {
+            tr.classList.add('select');
+         }
+      }
+      input.value = '';
    }
 }
