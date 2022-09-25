a8378761012ee1c6d1dcfa91ac4abc1b28891fbb
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-25 13:39:11 +0300

    Problem 3

--- Softuni - Advanced course/04. DOM Introduction/Exercises/03. Accordion/accordion.js
+++ Softuni - Advanced course/04. DOM Introduction/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    let myButton = document.getElementsByClassName("button")[0];
+    let myDiv = document.getElementById('extra');
+    console.log(myDiv);
+    console.log(myDiv.style.display);
+    if (myButton.textContent == `More`) {
+        myDiv.style.display = `block`;
+        myButton.textContent = `Less`;
+    } else if (myButton.textContent == `Less`) {
+        myDiv.style.display = `none`;
+        myButton.textContent = `More`;
+    }
 }
