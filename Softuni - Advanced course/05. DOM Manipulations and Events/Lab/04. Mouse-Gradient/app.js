96064942056d545b42a8148ab61d81e7c1a1eca9
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-28 14:15:52 +0300

    5

--- Softuni - Advanced course/05. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ Softuni - Advanced course/05. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -3,12 +3,11 @@ function attachGradientEvents() {
     gradient.addEventListener('mousemove', gradientMove);
     gradient.addEventListener('mouseout', gradientOut);
     function gradientMove(event) {
-      let power = event.offsetX / (event.target.clientWidth - 1);
-      power = Math.trunc(power * 100);
-      document.getElementById('result').textContent = power + "%";
+        let power = event.offsetX / (event.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + "%";
     }
     function gradientOut(event) {
-      document.getElementById('result').textContent = "";
+        document.getElementById('result').textContent = "";
     }
-  };
-  
+};
