c46c237da9abc68d41c8fe0f53c37d260789eea0
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-30 18:11:48 +0300

    Update script.js

--- Softuni - Advanced course/05. DOM Manipulations and Events/Exercises/01. Sections/script.js
+++ Softuni - Advanced course/05. DOM Manipulations and Events/Exercises/01. Sections/script.js
@@ -1,3 +1,16 @@
-function create(words) {
-   console.log('TODO:...');
+function create(words){
+  
+   for(let el of words){
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.innerText = el;
+      p.style.display = 'none';
+      div.appendChild(p);
+      div.addEventListener('click', showP)
+      document.getElementById('content').appendChild(div);
+   }
+
+   function showP(event){
+      event.target.children[0].style.display = 'block';
+   }
 }
