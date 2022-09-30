e5ae897d29863bdd729a4f930ef660e580576e00
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-30 14:08:40 +0300

    7

--- Softuni - Advanced course/05. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
+++ Softuni - Advanced course/05. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-   //TODO...
+   let shoppingCart = document.getElementsByClassName('shopping-cart')[0];
+   let textArea = document.getElementsByTagName('textarea')[0];
+
+   let products = [];
+   let totalPrice = 0;
+
+   let checkoutDone = false;
+
+   shoppingCart.addEventListener('click', function (event) {
+      if (event.target.nodeName !== 'BUTTON') {
+         return;
+      }
+
+      if(checkoutDone){
+         return;
+      }
+
+      let button = event.target;
+      if (Array.from(button.classList).indexOf('add-product') >= 0) {
+
+         let productElement = event.target.parentElement.parentElement;
+
+         let name = productElement.querySelectorAll('.product-title')[0].textContent;
+         let price = productElement.querySelectorAll('.product-line-price')[0].textContent;
+
+         textArea.textContent += `Added ${name} for ${price} to the cart.\n`
+
+         totalPrice += Number(price);
+
+         if (products.indexOf(name) < 0) {
+            products.push(name);
+
+         }
+
+
+      } else if (Array.from(button.classList).indexOf('checkout') >= 0) {
+         let list = products.join(', ');
+         textArea.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.`
+         checkoutDone = true;
+      }
+
+
+   });
 }
