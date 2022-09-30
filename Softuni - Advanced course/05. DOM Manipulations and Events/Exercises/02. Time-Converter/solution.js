c08bfbd46b18522a9164cbbb073cf42a65a73b73
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-09-30 22:32:26 +0300

    Update solution.js

--- Softuni - Advanced course/05. DOM Manipulations and Events/Exercises/02. Time-Converter/solution.js
+++ Softuni - Advanced course/05. DOM Manipulations and Events/Exercises/02. Time-Converter/solution.js
@@ -1,4 +1,27 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll('input[type=button]'));
+
+    for(let button of buttons){
+        button.addEventListener('click', convert);
+    }
+
+    function convert(e){
+        let value = Number(e.target.parentElement.querySelectorAll('input[type=text]').value);
+        let unit = e.target.id;
+
+        switch(unit){
+            case 'daysBtn': populate(value);
+            break;
+            case'hoursBtn': populate(value) / 24;
+            break;
+            case'minutesBtn': populate(value / 24 / 60);
+            break;
+            case'secondsBtn': populate(value / 24/ 60 / 60)
+            break;
+        }
+    }
+    function populate (value){
+
+    }
 }
