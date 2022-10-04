94929024981c342b8e5d740b3863ed5a228f6d54
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-04 20:45:51 +0300

    3

---
+++ Softuni - Advanced course/07.Unit Testing and Error Handling/Lab/03.deckOfCards.js
@@ -0,0 +1,44 @@
+function printDeckOfCards(cards) {
+
+    function playingCards(face, suit) {
+
+        const validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const validSuits = ['S', 'H', 'D', 'C'];
+
+        if (validFaces.indexOf(face.toString()) === -1) {
+            throw new Error('Error')
+        }
+
+        if (validSuits.indexOf(suit) === -1) {
+            throw new Error('Error')
+        }
+
+        switch (suit) {
+            case 'S': suit = '\u2660 '; break;
+            case 'H': suit = '\u2665 '; break;
+            case 'D': suit = '\u2666 '; break;
+            case 'C': suit = '\u2663 '; break;
+        }
+
+        return {
+            face: face,
+            suit: suit,
+            toString() {
+                return this.face + this.suit;
+            }
+        }
+
+    }
+
+    let buff = '';
+    for(let el of cards){
+        let face = el[0];
+        let suit = el[1];
+        
+        console.log(playingCards(face, suit));
+
+    }
+
+} 
+
+printDeckOfCards(['AS', '10D', 'KH', '2C'])
