8abb185fef28512029b0010ce58691bd7a40eae9
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-05 13:13:26 +0300

    Update script.js

--- Softuni - Advanced course/06.Advanced Functions/Exercises/08. Next Article/script.js
+++ Softuni - Advanced course/06.Advanced Functions/Exercises/08. Next Article/script.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    
+    let myArticle = Array.from(articles);
+    let content = document.getElementById('content');
+
+    return () => {
+        if(!myArticle.length){
+            return
+        };
+        let currentArticle = myArticle.shift();
+        content.innerHTML += `<article>${currentArticle}</article>`
+    }
 }
