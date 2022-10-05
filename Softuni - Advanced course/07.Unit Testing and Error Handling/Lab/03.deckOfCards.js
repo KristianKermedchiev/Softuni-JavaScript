c6930765b5674e6c81bbeda4178dcb6f97f0e0fc
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-05 12:30:36 +0300

    7

--- Softuni - Advanced course/07.Unit Testing and Error Handling/Lab/03.deckOfCards.js
+++ Softuni - Advanced course/07.Unit Testing and Error Handling/Lab/03.deckOfCards.js
@@ -1,16 +1,19 @@
 function printDeckOfCards(cards) {
+    let flag = true;
 
     function playingCards(face, suit) {
 
         const validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
         const validSuits = ['S', 'H', 'D', 'C'];
 
+       
+
         if (validFaces.indexOf(face.toString()) === -1) {
-            throw new Error('Error')
+            flag = false;
         }
 
         if (validSuits.indexOf(suit) === -1) {
-            throw new Error('Error')
+            flag = false;
         }
 
         switch (suit) {
@@ -32,13 +35,32 @@ function printDeckOfCards(cards) {
 
     let buff = '';
     for(let el of cards){
-        let face = el[0];
-        let suit = el[1];
-        
-        console.log(playingCards(face, suit));
+
+        if(el.length === 2){
+            let face = el[0];
+            let suit = el[1];
+            let temp = playingCards(face, suit);
+            if(!flag){
+                console.log(`Invalid card: ${temp.face + suit}`)
+                return;
+            }else {
+                buff += temp.face + temp.suit;
+            }  
+        } else if (el.length === 3){
+            let face = el[0] + el[1];
+            let suit = el[2];
+            let temp = playingCards(face, suit);
+            if(!flag){
+                console.log(`Invalid card: ${temp.face + suit}`)
+                return;
+            }else {
+                buff += temp.face + temp.suit;
+            } 
+        }
 
     }
-
+    console.log(buff)
 } 
 
-printDeckOfCards(['AS', '10D', 'KH', '2C'])
+printDeckOfCards(['AS', '10D', 'KH', '2C']);
+printDeckOfCards(['2C', '3D', 'QD', '1C']);
