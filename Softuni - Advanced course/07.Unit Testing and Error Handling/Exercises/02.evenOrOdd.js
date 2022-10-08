c532b8d028e4d4561b60da1a3485cb7b83f86351
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-08 12:36:09 +0300

    2 & 3

--- Softuni - Advanced course/07.Unit Testing and Error Handling/Exercises/02.evenOrOdd.js
+++ Softuni - Advanced course/07.Unit Testing and Error Handling/Exercises/02.evenOrOdd.js
@@ -7,3 +7,5 @@ function isOddOrEven(string) {
     }
     return "odd";
 }
+
+module.exports = { isOddOrEven };
---
+++ Softuni - Advanced course/07.Unit Testing and Error Handling/Exercises/test/02.evenOrOdd.test.js
@@ -0,0 +1,44 @@
+let { assert } = require('chai');
+let { isOddOrEven } = require('../02.evenOrOdd');
+
+describe ('Tests for evenOrOdd', () => {
+    it('Should return undefined if input is an array', ()=>{
+        assert.equal(isOddOrEven([]), undefined);
+    });
+    it('Should return undefined if input is NaN', ()=>{
+        assert.equal(isOddOrEven(NaN), undefined);
+    });
+    it('Should return undefined if input is Number', ()=>{
+        assert.equal(isOddOrEven(9), undefined);
+    });
+    it('Should return undefined if input is Object', ()=>{
+        assert.equal(isOddOrEven({}), undefined);
+    });
+
+
+    it('Should return even, if given one digit numbers as String', () =>{
+        assert.equal(isOddOrEven('2'), 'odd');
+    });
+
+    it('Should return even, if given one digit numbers as String', () =>{
+        assert.equal(isOddOrEven('3'), 'odd');
+    });
+
+    it('Should return even, if given one digit numbers as String', () =>{
+        assert.equal(isOddOrEven('7'), 'odd');
+    });
+
+
+
+    it('Should return even, if given 4 digit numbers as String', () =>{
+        assert.equal(isOddOrEven('3333'), 'even');
+    });
+
+    it('Should return even, if given six digit numbers as String', () =>{
+        assert.equal(isOddOrEven('777777'), 'even');
+    });
+
+    it('Should return even, if given eight digit numbers as String', () =>{
+        assert.equal(isOddOrEven('99999999'), 'even');
+    });
+});
