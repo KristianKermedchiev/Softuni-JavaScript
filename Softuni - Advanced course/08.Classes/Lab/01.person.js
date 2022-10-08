e824b5967a6c200ee058a4567ed8ebbf74c6d65e
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-08 16:18:38 +0300

    Create 01.person.js

---
+++ Softuni - Advanced course/08.Classes/Lab/01.person.js
@@ -0,0 +1,20 @@
+class Person  {
+    constructor(firstName, lastName, age, email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+    
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+    }
+
+};
+
+let person = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+console.log(person.toString());
+
+let p = new Person("Peter", "Marinov", 18, "pesho18@abv.bg");
+let str = p.toString();
+console.log(str);
