a12eb8e02ae5aedd02ebf84eb3cce6e05132209c
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-08 14:29:45 +0300

    6

--- Softuni - Advanced course/07.Unit Testing and Error Handling/Exercises/06.Dynamic-Validation/app.js
+++ Softuni - Advanced course/07.Unit Testing and Error Handling/Exercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    let  input = document.getElementById('email');
+    input.addEventListener('change', onChange);
+
+    function onChange(event){
+        let email = event.target.value;
+        let pattern = /[A-Za-z0_9]+@[A-Za-z0_9]+\.[A-Za-z]+/g;
+
+        if(pattern.test(email)){
+            event.target.classList.remove('error')
+        } else {
+            event.target.classList.add('error');
+        }
+    }
 }
