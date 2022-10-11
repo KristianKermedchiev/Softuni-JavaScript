bb2f2c32622369de64096778c0e574db92aacc01
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-11 18:47:29 +0300

    Create 04.extendProtorype.js

---
+++ Softuni - Advanced course/09.Prototypes and Inheritance/Lab/04.extendProtorype.js
@@ -0,0 +1,6 @@
+function extendPrototy (classToExtend){
+    classToExtend.prototype.species = 'Human';
+    classToExtend.prototype.toSpeciesString = function () {
+        return `I am a ${this.species}. ${this.toString()}`
+    }
+}
