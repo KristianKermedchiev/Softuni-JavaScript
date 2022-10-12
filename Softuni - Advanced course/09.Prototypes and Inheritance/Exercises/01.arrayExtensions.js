77fcaf8acb55b06d3a59936a65df9c40711b583d
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-12 13:49:02 +0300

    Create 01.arrayExtensions.js

---
+++ Softuni - Advanced course/09.Prototypes and Inheritance/Exercises/01.arrayExtensions.js
@@ -0,0 +1,22 @@
+(function arrayExtensions () {
+
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+
+    Array.prototype.skip = function (n) {
+        return this.slice(n);
+    };
+
+    Array.prototype.take = function (n) {
+        return this.slice(0, n);
+    }
+    
+    Array.prototype.sum = function() {
+        return this.reduce((prevValue, nextValue) => prevValue + nextValue);
+    };
+
+    Array.prototype.average = function() {
+        return this.sum() / this.length;
+    }
+})()
