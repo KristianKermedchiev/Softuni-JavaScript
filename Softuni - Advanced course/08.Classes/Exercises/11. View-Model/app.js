c11e7cc1d441ffc1942bdafb1b2996d2c95e6f32
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-16 01:36:43 +0300

    Solutions

--- Softuni - Advanced course/08.Classes/Exercises/11. View-Model/app.js
+++ Softuni - Advanced course/08.Classes/Exercises/11. View-Model/app.js
@@ -1,8 +1,31 @@
 class Textbox {
-    //TODO
+    constructor(selector, regex) {
+        this._value = "";
+        this._elements = Array.from(document.querySelectorAll(selector));
+        this._invalidSymbols = regex;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(v) {
+        this._value = v;
+        for (let index = 0; index < this._elements.length; index++) {
+            this._elements[index].value = v;
+        }
+    }
+
+    get elements() {
+        return this._elements;
+    }
+
+    isValid() {
+        return !this._invalidSymbols.test(this.value);
+    }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
 let inputs = document.getElementsByClassName('.textbox');
 
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+inputs.addEventListener('click', function () { console.log(textbox.value); });
