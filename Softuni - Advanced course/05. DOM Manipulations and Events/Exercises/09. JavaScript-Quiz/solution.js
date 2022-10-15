d363cd5001ad7d3a67eb8e82d477a2f8a98bd0f4
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2022-10-16 01:02:33 +0300

    9

--- Softuni - Advanced course/05. DOM Manipulations and Events/Exercises/09. JavaScript-Quiz/solution.js
+++ Softuni - Advanced course/05. DOM Manipulations and Events/Exercises/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-  //TODO...
+  let correctAnswers = [
+    "onclick",
+    "JSON.stringify()",
+    "A programming API for HTML and XML documents",
+  ];
+  let rightClick = 0;
+  let index = 0;
+
+  let result = document.querySelector("#results");
+  let resH = document.querySelector("#results h1");
+
+  Array.from(document.getElementsByClassName("answer-text")).map((x) =>
+    x.addEventListener("click", answerClick)
+  );
+
+  function answerClick(event) {
+    if (correctAnswers.includes(event.target.textContent)) {
+      rightClick++;
+    }
+
+    let currSection = document.querySelectorAll("section")[index];
+    currSection.style.display = "none";
+
+    if (document.querySelectorAll("section")[index + 1]) {
+      let nextSection = document.querySelectorAll("section")[index + 1];
+      nextSection.style.display = "block";
+      index++;
+    } else {
+      result.style.display = "block";
+
+      rightClick === 3
+        ? (resH.innerHTML = "You are recognized as top JavaScript fan!")
+        : (resH.innerHTML = `You have ${rightClick} right answers`);
+    }
+  }
 }
