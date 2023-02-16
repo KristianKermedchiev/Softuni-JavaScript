a1ecb7abeb3df4cf0f0c29d4e4c20587c46f85ed
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-02-16 13:46:51 +0200

    Book talk

---
+++ Softuni - Back-End course/Book Talk/utils/errorUtils.js
@@ -0,0 +1,18 @@
+function getFirstMongooseError(error) {
+    const firstError = Object.values(error.errors)[0].message;
+
+    return firstError;
+};
+
+exports.getErrorMessage = (error) => {
+    console.log(error)
+    switch(error.name){
+        case 'Error':
+            return error.message;
+            case 'ValidationError':
+                return getFirstMongooseError(error);
+                default:
+                    return error.message;
+    };
+};
+
