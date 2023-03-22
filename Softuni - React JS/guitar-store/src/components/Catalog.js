56a7af7d1fddf52cd7fe85f08aed73e98e37ecae
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-03-22 17:43:53 +0200

    Catalog page HTML and CSS

--- Softuni - React JS/guitar-store/src/components/Catalog.js
+++ Softuni - React JS/guitar-store/src/components/Catalog.js
@@ -1,9 +1,46 @@
+import '../styles/Catalog.css';
+
 function Catalog() {
     return (
-      <div>
-        <h1>Catalog Page</h1>
-        <p>Welcome to the Catalog page!</p>
+      <div class="catalog-container">
+      <div class="catalog-item">
+        <img src="item1.jpg" alt="Item 1"></img>
+        <h2>Item 1 Name</h2>
       </div>
+      <div class="catalog-item">
+        <img src="item2.jpg" alt="Item 2"></img>
+        <h2>Item 2 Name</h2>
+      </div>
+      <div class="catalog-item">
+        <img src="item3.jpg" alt="Item 3"></img>
+        <h2>Item 3 Name</h2>
+      </div>
+      <div class="catalog-item">
+        <img src="item4.jpg" alt="Item 4"></img>
+        <h2>Item 4 Name</h2>
+      </div>
+      <div class="catalog-item">
+        <img src="item5.jpg" alt="Item 5"></img>
+        <h2>Item 5 Name</h2>
+      </div>
+      <div class="catalog-item">
+        <img src="item6.jpg" alt="Item 6"></img>
+        <h2>Item 6 Name</h2>
+      </div>
+      <div class="catalog-item">
+        <img src="item7.jpg" alt="Item 7"></img>
+        <h2>Item 7 Name</h2>
+      </div>
+      <div class="catalog-item">
+        <img src="item8.jpg" alt="Item 8"></img>
+        <h2>Item 8 Name</h2>
+      </div>
+      <div class="catalog-item">
+        <img src="item9.jpg" alt="Item 9"></img>
+        <h2>Item 9 Name</h2>
+      </div>
+    </div>
+    // pagination goes here
     );
   }
   
