99f2277eca1a944e7cf42fc4b22c82f4ec1a7894
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-03-24 14:16:33 +0200

    Contact US css and js

--- Softuni - React JS/guitar-store/src/components/ContactUs.js
+++ Softuni - React JS/guitar-store/src/components/ContactUs.js
@@ -1,10 +1,44 @@
-function ContactUs() {
+import React from 'react';
+import '../styles/ContactUs.css'
+
+class ContactUs extends React.Component {
+  render() {
     return (
       <div>
-        <h1>ContactUs Page</h1>
-        <p>Welcome to the ContactUs page!</p>
+        <h1 className='contact-us-h1'>Contact Us</h1>
+      <div className='contactUs-container'>
+        <div className='table-holder'>
+          <table>
+            <tbody>
+              <tr>
+                <th>Address:</th>
+                <td>Sofia, Bulgaria 1000</td>
+              </tr>
+              <tr>
+                <th>Phone:</th>
+                <td>08865432198</td>
+              </tr>
+              <tr>
+                <th>Email:</th>
+                <td>elon.musk@abv.bg</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className='map-holder'>
+          <iframe
+            title="Google Maps"
+            src="https://www.google.com/maps/embed/v1/place?q=42.64074483181473, 23.371297331013057&key=AIzaSyCASIsDVvkeFxvhVQv7IAxax7wdKo_dO_Q"
+            frameborder="0"
+            style={{ border: '0', width: '100%', height: '100%' }}
+            allowfullscreen
+            loading="lazy"
+          ></iframe>
+        </div>
+        </div>
       </div>
     );
   }
-  
-  export default ContactUs;
+}
+
+export default ContactUs;
